d1e46c7548a55a88c9f5441a1ce522e6e171b174
khuongsatou <yoshi240499@gmail.com>
2019-11-24 14:36:03 +0700

    vesion 1.0 masterpage+register

---
+++ Rau/FoodRau/Admin/Register.aspx.cs
@@ -0,0 +1,57 @@
+﻿using FoodRau.HttpCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FoodRau.Admin
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_register_Click(object sender, EventArgs e)
+        {
+            
+            if (Convert.ToInt32(ddl_user.SelectedValue) < 0)
+            {
+                Response.Write("<script>alert('Bạn Chưa chọn user') </script>");
+                return;
+            }
+            if (Convert.ToInt32(ddl_status.SelectedValue) < 0)
+            {
+                Response.Write("<script>alert('Bạn Chưa chọn status') </script>");
+                return;
+            }
+
+            Member member = new Member();
+            //nếu trả về false là chưa tồn tại
+            if (!member.existMember(txtUserName.Text))
+            {
+                member.UserName = txtUserName.Text;
+                member.Name = txtName.Text;
+                member.Pass = txtPassword.Text;
+                member.Email = txtEmail.Text;
+                member.Phone = txtPhone.Text;
+                member.Role = Convert.ToInt32(ddl_user.SelectedValue);
+                member.Status = Convert.ToInt32(ddl_status.SelectedValue);
+                //thêm vào trả về số dòng tác động , thành công 1(true) , thất bại 0(false)
+                if (member.addMember())
+                {
+                    //in ra hộp thoại thông báo.
+                    Response.Write("<script>alert('Thành Công') </script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Thất Bại') </script>");
+                }
+            }
+            
+        }
+    }
+}
---
+++ Rau/FoodRau/HttpCode/Member.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FoodRau.HttpCode
+{
+    public class Member
+    {
+        private string _userName;
+        private string _pass;
+        private string _name;
+        private string _email;
+        private string _phone;
+        private int _role;
+        private int _status;
+
+        public Member()
+        {
+            _role = 0;
+            Status = 0;
+        }
+
+        public Member(string userName, string pass, string name, string email, string phone, int role)
+        {
+            UserName = userName;
+            Pass = pass;
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Role = role;
+        }
+
+        public string UserName { get => _userName; set => _userName = value; }
+        public string Pass { get => _pass; set => _pass = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Phone { get => _phone; set => _phone = value; }
+        public int Role { get => _role; set => _role = value; }
+        public int Status { get => _status; set => _status = value; }
+
+        public bool existMember(string username)
+        {
+            string sQuery = "SELECT [username] FROM [dbo].[member] WHERE [username] = '@username'";
+            SqlParameter[] param =
+            {
+                //có 2 cách 1 là truyền đối tượng vào insert
+                //cách 1: nhưng phải set cho nó trước
+                //new SqlParameter("@username",member.UserName)
+                //đây là cách 2: lấy username đó tìm
+                new SqlParameter("@username",username)
+            };
+            //trả về true(1) hoặc false(0)
+            return DataProvider.executeNonQuery(sQuery, param);
+        }
+
+        public bool addMember()
+        {
+            string sQuery = "INSERT INTO [dbo].[member] ([username] ,[pass] ,[name] ,[email] ,[phone] ,[role],[status]) VALUES (@username,@pass,@name,@email,@phone,@role,@status)";
+            SqlParameter[] param =
+            {
+                new SqlParameter("@username",this._userName),
+                new SqlParameter("@pass",this._pass),
+                new SqlParameter("@name",this._name),
+                new SqlParameter("@email",this._email),
+                new SqlParameter("@phone",this._phone),
+                new SqlParameter("@role",this._role),
+                new SqlParameter("@status",this.Status)
+            };
+            //trả về true(1) hoặc false(0)
+            return DataProvider.executeNonQuery(sQuery, param);
+        }
+
+
+    }
+}
